8167d212089f877a50a0f26e64fd9a6d75eb13e6
luke_stephens_ <lukasds2@illinois.edu>
2020-04-28 23:41:05 -0500

    added page to preset virus added back to mains will begin research

--- app/src/main/java/com/example/pandemicsimulator/activity_existing_virus.java
+++ app/src/main/java/com/example/pandemicsimulator/activity_existing_virus.java
@@ -2,13 +2,31 @@ package com.example.pandemicsimulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class activity_existing_virus extends AppCompatActivity {
+
+    Button backtoMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_existing_virus);
+
+
+        backtoMain = (Button) findViewById(R.id.backtoMainpage);
+        backtoMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                goingBacktoMainpage();
+            }
+        });
+    }
+    private void goingBacktoMainpage() {
+        Intent backtoMainpage = new Intent(this, MainActivity.class);
+        startActivity(backtoMainpage);
     }
 }
